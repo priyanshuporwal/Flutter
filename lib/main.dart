444d370ac273ca83d2cf26ecb9100566d1e6856c
Spy <priyanshuporwal1999@gmail.com>
2022-08-11 23:55:32 +0530

    ch2 Complete

--- lib/main.dart
+++ lib/main.dart
@@ -1,50 +1,57 @@
-// ignore_for_file: prefer_const_constructors
-
 import 'package:flutter/material.dart';
-//import 'package:flutter/cupertino.dart'; //For IOS APP
 
 void main() {
   runApp(MaterialApp(
-    title: "My App",
+    title: "Awesome App",
     home: HomePage(),
   ));
 }
 
-// ignore: use_key_in_widget_constructors
 class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          // ignore: prefer_const
-          title: Text("Hello Spy"),
-        ),
-        // ignore: avoid_unnecessary_containers
-        body: Center(
+      appBar: AppBar(
+        title: Text("Awesome App"),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Align(
+          alignment: Alignment.bottomRight,
           child: Container(
-            padding: const EdgeInsets.all(8),
-            alignment: Alignment.center,
-            width: 100,
-            height: 100,
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(10),
-              boxShadow: const [
-                BoxShadow(
-                    color: Colors.grey, blurRadius: 5, offset: Offset(2.0, 5.0))
-              ],
-              color: Colors.teal,
-              gradient: LinearGradient(colors: [Colors.yellow, Colors.red]),
-            ),
-            child: Text(
-              "I'm a Box",
-              textAlign: TextAlign.center,
-              style: TextStyle(
-                color: Colors.white,
-                fontWeight: FontWeight.bold,
-                fontSize: 20,
+            color: Colors.black,
+            width: 200,
+            height: 400,
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: <Widget>[
+                  Container(
+                    padding: const EdgeInsets.all(8),
+                    width: 100,
+                    height: 100,
+                    color: Colors.green,
+                  ),
+                  Container(
+                    padding: const EdgeInsets.all(8),
+                    width: 100,
+                    height: 100,
+                    color: Colors.yellow,
+                  ),
+                  Container(
+                    padding: const EdgeInsets.all(8),
+                    width: 100,
+                    height: 100,
+                    color: Colors.red,
+                  )
+                ],
               ),
             ),
           ),
-        ));
+        ),
+      ),
+    );
   }
 }
