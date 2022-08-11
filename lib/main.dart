1c031b6e724467f4ff7ea3862372749f823ad3a9
Spy <priyanshuporwal1999@gmail.com>
2022-08-11 23:38:56 +0530

    ch3 complete

--- lib/main.dart
+++ lib/main.dart
@@ -15,14 +15,36 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        // ignore: prefer_const_constructors
-        title: Text("Hello Spy"),
-      ),
-      // ignore: avoid_unnecessary_containers
-      body: Container(
-        child: Center(child: Text("Hi Flutter")),
-      ),
-    );
+        appBar: AppBar(
+          // ignore: prefer_const
+          title: Text("Hello Spy"),
+        ),
+        // ignore: avoid_unnecessary_containers
+        body: Center(
+          child: Container(
+            padding: const EdgeInsets.all(8),
+            alignment: Alignment.center,
+            width: 100,
+            height: 100,
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(10),
+              boxShadow: const [
+                BoxShadow(
+                    color: Colors.grey, blurRadius: 5, offset: Offset(2.0, 5.0))
+              ],
+              color: Colors.teal,
+              gradient: LinearGradient(colors: [Colors.yellow, Colors.red]),
+            ),
+            child: Text(
+              "I'm a Box",
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                color: Colors.white,
+                fontWeight: FontWeight.bold,
+                fontSize: 20,
+              ),
+            ),
+          ),
+        ));
   }
 }
